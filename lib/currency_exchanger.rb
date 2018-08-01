56a48e9594c35dc9532f92024e18b2757a186c2c
Paula Muldoon <pmuldoon@umich.edu>
2018-08-01 21:36:29 +0100

    initial commit

---
+++ lib/currency_exchanger.rb
@@ -0,0 +1,5 @@
+require "currency_exchanger/version"
+
+module CurrencyExchanger
+  # Your code goes here...
+end
---
+++ spec/currency_exchanger_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CurrencyExchanger do
+  it "has a version number" do
+    expect(CurrencyExchanger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
