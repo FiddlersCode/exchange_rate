d98017356bfa60e40d5b41d2f441bb3dfa995d75
Paula Muldoon <pmuldoon@umich.edu>
2018-08-04 12:30:55 +0100

    dry out tests

--- spec/currency_exchanger_spec.rb
+++ spec/currency_exchanger_spec.rb
@@ -5,33 +5,29 @@ RSpec.describe CurrencyExchanger do
     expect(CurrencyExchanger::VERSION).to eq '0.1.0'
   end
 
-  describe 'instantiation' do
-    it 'currency exchanger accepts a from currency' do
-      currency_exchanger = CurrencyExchanger.new('USD', 'CHF', 'today')
-      expect(currency_exchanger.from_currency).to eq('USD')
-    end
+  before(:each) do
+    @currency_exchanger = CurrencyExchanger.new('USD', 'CHF', Date.today)
+  end
 
-    it 'currency exchanger accepts a to currency' do
-      currency_exchanger = CurrencyExchanger.new('USD', 'CHF', 'today')
-      expect(currency_exchanger.to_currency).to eq('CHF')
+  describe 'instantiation' do
+    it 'currency exchanger accepts from and to currencies' do
+      expect(@currency_exchanger.from_currency).to eq('USD')
+      expect(@currency_exchanger.to_currency).to eq('CHF')
     end
 
     it 'currency exchanger accepts a date' do
-      currency_exchanger = CurrencyExchanger.new('USD', 'CHF', 'today')
-      expect(currency_exchanger.date).to eq('today')
+      expect(@currency_exchanger.date).to eq(Date.today)
     end
   end
 
   describe 'get exchange rate' do
     it 'returns a new exchange rate object' do
-      currency_exchanger = CurrencyExchanger.new('USD', 'USD', 'today')
-      expect(currency_exchanger.get_exchange_rate).to eq(ExchangeRate.new(currency_exchanger.to_currency, currency_exchanger.from_currency, currency_exchanger.date))
+      expect(@currency_exchanger.get_exchange_rate).to eq(ExchangeRate.new(@currency_exchanger.from_currency, @currency_exchanger.to_currency, @currency_exchanger.date))
     end
 
     it 'returns a new exchange rate object with a new date object set to today\'s date' do
-      currency_exchanger = CurrencyExchanger.new('USD', 'USD', Date.today)
-      exchange_rate_object = currency_exchanger.get_exchange_rate
-      expect(exchange_rate_object.date).to eq Date.today
+      @exchange_rate_object = @currency_exchanger.get_exchange_rate
+      expect(@exchange_rate_object.date).to eq Date.today
     end
 
     it 'returns a new exchange rate object with a new date object set to a past date' do
