698854acb1283de62a0f1d4e2f592e5c47cecb05
Paula Muldoon <pmuldoon@umich.edu>
2018-08-04 09:44:49 +0100

    add tests for instantiation with from/to currencies and date

--- lib/currency_exchanger/currency_exchanger.rb
+++ lib/currency_exchanger/currency_exchanger.rb
@@ -1,4 +1,13 @@
 class CurrencyExchanger
+
+  attr_reader :from_currency, :to_currency, :date
+
+  def initialize(from_currency, to_currency, date)
+    @from_currency = from_currency
+    @to_currency = to_currency
+    @date = date
+  end
+
   def get_exchange_rate
     1
   end
--- spec/currency_exchanger_spec.rb
+++ spec/currency_exchanger_spec.rb
@@ -5,7 +5,22 @@ RSpec.describe CurrencyExchanger do
   end
 
   it "can get exchange rate" do
-    currency_exchanger = CurrencyExchanger.new
+    currency_exchanger = CurrencyExchanger.new("USD", "USD", "today")
     expect(currency_exchanger.get_exchange_rate).to eq(1)
+  end
+
+  it "currency exchanger accepts a from currency" do
+    currency_exchanger = CurrencyExchanger.new("USD", "CHF", "today")
+    expect(currency_exchanger.from_currency).to eq("USD")
+  end
+
+  it "currency exchanger accepts a to currency" do
+    currency_exchanger = CurrencyExchanger.new("USD", "CHF", "today")
+    expect(currency_exchanger.to_currency).to eq("CHF")
+  end
+
+  it "currency exchanger accepts a date" do
+    currency_exchanger = CurrencyExchanger.new("USD", "CHF", "today")
+    expect(currency_exchanger.date).to eq("today")
   end
 end
