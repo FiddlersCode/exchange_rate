1eff13fe27df664a46ace782c3382af974c2a944
Paula Muldoon <pmuldoon@umich.edu>
2018-08-04 14:20:00 +0100

    add capybara to file fetch test

--- spec/xml_parser_spec.rb
+++ spec/xml_parser_spec.rb
@@ -2,9 +2,13 @@ require 'exchange_rate'
 
 RSpec.describe ExchangeRate do
 
-  it 'fetches the document and stores it locally' do
-    exchange_rate_xml = (open('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml?526da90c43e51cc5b44bf360e6f1dac3'))
-    IO.copy_stream(exchange_rate_xml, "./lib/rates/daily_rates.xml")
-    expect(File.exist?("./lib/rates/daily_rates.xml")).to be true
+  describe 'file fetching', type: :feature do
+    Capybara.current_driver = :selenium
+    it 'fetches the document and stores it locally' do
+      exchange_rate_xml = (open('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml?526da90c43e51cc5b44bf360e6f1dac3'))
+      IO.copy_stream(exchange_rate_xml, "./lib/rates/daily_rates.xml")
+      expect(File.exist?("./lib/rates/daily_rates.xml")).to be true
+    end
+
   end
 end
