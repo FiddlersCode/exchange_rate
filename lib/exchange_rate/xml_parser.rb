cd64fe0477e16fdd71d23f90de07d3940f55e373
Paula Muldoon <pmuldoon@umich.edu>
2018-08-04 19:54:00 +0100

    use different xpath selector to remove array indexing

--- lib/exchange_rate/xml_parser.rb
+++ lib/exchange_rate/xml_parser.rb
@@ -6,7 +6,7 @@ class XMLParser
   def get_rate(exchange_rate_object)
     doc = Nokogiri::XML(File.open(@file))
     self.format_xml(doc)
-    doc.xpath("//Cube/Cube/Cube/@rate")[0].to_s
+    doc.xpath('//Cube/Cube[@time="2018-08-03"]/Cube[@currency="USD"]/@rate').to_s
   end
 
 
