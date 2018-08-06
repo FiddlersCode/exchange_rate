59f3b894136eca49d589e899488a01225a94d2ce
Paula Muldoon <pmuldoon@umich.edu>
2018-08-06 15:47:26 +0100

    add test xml file for xml parser tests

--- spec/xml_parser_spec.rb
+++ spec/xml_parser_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe ExchangeRate do
 
   describe 'file fetching', type: :feature do
     Capybara.current_driver = :selenium
-    xit 'fetches the document and stores it locally' do
+    it 'fetches the document and stores it locally' do
       exchange_rate_xml = (open('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml?526da90c43e51cc5b44bf360e6f1dac3'))
-      IO.copy_stream(exchange_rate_xml, "./lib/rates/daily_rates.xml")
-      expect(File.exist?("./lib/rates/daily_rates.xml")).to be true
+      IO.copy_stream(exchange_rate_xml, "./spec/test_data/test_daily_rate.xml")
+      expect(File.exist?("./spec/test_data/test_daily_rate.xml")).to be true
     end
 
   end
@@ -15,7 +15,7 @@ RSpec.describe ExchangeRate do
   describe 'parsing the xml' do
 
     it 'should get the date of the first item in the row' do
-      parser = XMLParser.new("./lib/rates/daily_rates.xml")
+      parser = XMLParser.new("./spec/test_data/test_daily_rate.xml")
       expect(parser.get_rate('2018-08-03', 'USD')).to eq 1.1588
     end
   end
