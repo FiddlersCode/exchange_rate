126709c574f173d67c9e539c9c3dba1f803a08ed
Paula Muldoon <pmuldoon@umich.edu>
2018-08-06 17:27:12 +0100

    add data retriever class and remove data retrieval tests from xml parser tests

--- spec/dev/xml_parser_spec.rb
+++ spec/dev/xml_parser_spec.rb
@@ -2,15 +2,6 @@ require 'exchange_rate'
 
 RSpec.describe ExchangeRate do
 
-  describe 'file fetching', type: :feature do
-    Capybara.current_driver = :selenium
-    it 'fetches the document and stores it locally' do
-      exchange_rate_xml = (open('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml?526da90c43e51cc5b44bf360e6f1dac3'))
-      IO.copy_stream(exchange_rate_xml, "./spec/test_data/daily_rates.xml")
-      expect(File.exist?("./spec/test_data/daily_rates.xml")).to be true
-    end
-
-  end
 
   describe 'parsing the xml' do
     before(:each) do
--- spec/integration/data_retriever_spec.rb
+++ spec/integration/data_retriever_spec.rb
@@ -8,12 +8,15 @@ RSpec.describe ExchangeRate do
       @retriever = DataRetriever.new
     end
 
-    it 'fetches the production xml and stores it locally' do
+    it 'fetches the dev xml and stores it locally' do
       url = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml?526da90c43e51cc5b44bf360e6f1dac3'
-      destination = './lib/rates/'
+      destination = './spec/test_data/'
       @retriever.get_rates_file(url, destination)
-      expect(File.exist?('./lib/rates/daily_rates.xml')).to be true
-      end
+      expect(File.exist?('./spec/test_data/daily_rates.xml')).to be true
     end
 
+  end
+
+
 end
+
