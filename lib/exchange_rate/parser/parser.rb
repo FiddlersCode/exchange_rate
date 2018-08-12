5cd4f1af688c4b02050ef376b40a56bdd70af532
Paula Muldoon <pmuldoon@umich.edu>
2018-08-12 20:22:52 +0100

    move relevant methods to subclasses of parser

--- lib/exchange_rate/parser/parser.rb
+++ lib/exchange_rate/parser/parser.rb
@@ -5,14 +5,6 @@ class Parser
     @file
   end
 
-  def get_rate(exchange_rate_object)
-    raise 'Date must be within the past 90 days.' unless date_valid?(exchange_rate_object.date)
-    raise "The base currency #{exchange_rate_object.base_currency} is not supported." unless base_currency_valid?(exchange_rate_object)
-    raise "The counter currency #{exchange_rate_object.counter_currency} is not supported." unless counter_currency_valid?(exchange_rate_object)
-
-    open_rates_file.xpath("//Cube/Cube[@time='#{exchange_rate_object.date}']/Cube[@currency='#{exchange_rate_object.counter_currency}']/@rate").to_s.to_f
-  end
-
   private
 
   def date_valid?(date)
@@ -25,21 +17,6 @@ class Parser
 
   def counter_currency_valid?(exchange_rate_object)
     true if get_counter_currencies(exchange_rate_object).include? exchange_rate_object.counter_currency
-  end
-
-  def get_counter_currencies(exchange_rate_object)
-    date_cubes = open_rates_file.xpath("//Cube/Cube[@time='#{exchange_rate_object.date}']/Cube")
-    date_cubes.to_a.map { |e| e.to_a[0][1] }
-  end
-
-  def open_rates_file
-    @file = get_current_rates_file
-    doc = Nokogiri::XML(File.open(@file))
-    format_xml(doc)
-  end
-
-  def format_xml(file)
-    file.remove_namespaces!
   end
 
 end
--- lib/exchange_rate/parser/xml_parser.rb
+++ lib/exchange_rate/parser/xml_parser.rb
@@ -1,4 +1,28 @@
 class XMLParser < Parser
+
+  def get_rate(exchange_rate_object)
+    raise 'Date must be within the past 90 days.' unless date_valid?(exchange_rate_object.date)
+    raise "The base currency #{exchange_rate_object.base_currency} is not supported." unless base_currency_valid?(exchange_rate_object)
+    raise "The counter currency #{exchange_rate_object.counter_currency} is not supported." unless counter_currency_valid?(exchange_rate_object)
+
+    open_rates_file.xpath("//Cube/Cube[@time='#{exchange_rate_object.date}']/Cube[@currency='#{exchange_rate_object.counter_currency}']/@rate").to_s.to_f
+  end
+
+  def get_counter_currencies(exchange_rate_object)
+    date_cubes = open_rates_file.xpath("//Cube/Cube[@time='#{exchange_rate_object.date}']/Cube")
+    date_cubes.to_a.map { |e| e.to_a[0][1] }
+  end
+
+  def open_rates_file
+    @file = get_current_rates_file
+    doc = Nokogiri::XML(File.open(@file))
+    format_xml(doc)
+  end
+
+  def format_xml(file)
+    file.remove_namespaces!
+  end
+
   def get_current_rates_file
     './spec/test_data/daily_rates.xml'
   end
