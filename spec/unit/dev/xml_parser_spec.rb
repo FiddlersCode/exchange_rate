cc887a4417063195a24ec85b46fc8de24f39d563
Paula Muldoon <pmuldoon@umich.edu>
2018-08-12 18:10:49 +0100

    change date in xml dev spec to fix failing unit test

--- spec/unit/dev/xml_parser_spec.rb
+++ spec/unit/dev/xml_parser_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ExchangeRate do
     end
 
     it 'should set the xml file' do
-      @exchange_rate = ExchangeRate.new(@yesterday, 'EUR', 'USD')
+      @exchange_rate = ExchangeRate.new(Date.new(2018,8,3), 'EUR', 'USD')
       @parser.get_rate(@exchange_rate)
       expect(@parser.file).to eq './spec/test_data/daily_rates.xml'
     end
