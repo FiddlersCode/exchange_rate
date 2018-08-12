aae1842d63ad55872ae66c67c67c77d422fa63ad
Paula Muldoon <pmuldoon@umich.edu>
2018-08-12 18:39:31 +0100

    add separate filepath and data cleanup for integration test

--- spec/integration/data_retriever_spec.rb
+++ spec/integration/data_retriever_spec.rb
@@ -8,9 +8,14 @@ RSpec.describe ExchangeRate do
 
     it 'fetches the dev xml and stores it locally' do
       url = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml?526da90c43e51cc5b44bf360e6f1dac3'
-      destination = './spec/test_data/'
+      destination = './spec/integration'
       @retriever.get_rates_file(url, destination)
-      expect(File.exist?('./spec/test_data/daily_rates.xml')).to be true
+      expect(File.exist?('./spec/integration/daily_rates.xml')).to be true
     end
+
+    after(:each) do
+      File.delete('./spec/integration/daily_rates.xml')
+    end
+
   end
 end
